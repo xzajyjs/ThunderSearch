7c36e8c5c62b1045923a5a8b98905d57e2b1d463
xzajyjs <xuziang16@gmail.com>
2022-01-25 18:42:27 +0800

    update to v1.5

--- ThunderSearch.py
+++ ThunderSearch.py
@@ -1,5 +1,5 @@
 import json
-from statistics import mode
+from pandas import wide_to_long
 import requests
 from tkinter import *
 from tkinter import messagebox
@@ -14,48 +14,56 @@ class Application(Frame):
         self.pack()
         
     def createWidget(self):
-        self.u_label = Label(self, text='账号').grid(row=0,column=0)
-        self.p_label = Label(self, text='密码').grid(row=1,column=0)
+        self.login_mode_label = Label(self,text='登陆方式',width=10).grid(row=0,column=0)
+        self.login_mode_choice = StringVar(self)
+        self.login_mode_choice.set('账号密码')
+        self.LOGIN_MODE = OptionMenu(self,self.login_mode_choice,'账号密码','API-KEY')
+        self.LOGIN_MODE.grid(row=1,column=0)
+        self.api_label = Label(self,text='API-KEY:').grid(row=2,column=0)
+        self.API = Entry(self,width=21,borderwidth=1)
+        self.API.grid(row=2,column=1,columnspan=2)
+        self.u_label = Label(self, text='账号:').grid(row=0,column=1)
+        self.p_label = Label(self, text='密码:').grid(row=1,column=1)
         self.USERNAME = Entry(self,width=16,borderwidth=1)
-        self.USERNAME.grid(row=0,column=1)
+        self.USERNAME.grid(row=0,column=2)
         self.PASSWORD = Entry(self,width=16,show="*",borderwidth=1)
-        self.PASSWORD.grid(row=1,column=1)
-        self.thread_label = Label(self, text='线程数').grid(row=0,column=2)
-        self.query_label = Label(self, text='查询语句').grid(row=1,column=2)
+        self.PASSWORD.grid(row=1,column=2)
+        self.thread_label = Label(self, text='线程数:',width=6).grid(row=0,column=3)
+        self.query_label = Label(self, text='查询语句:').grid(row=1,column=3)
         self.thread_choice = StringVar(self)
         self.thread_choice.set('5')
         self.THREAD = OptionMenu(self,self.thread_choice,'1','5','10','20','30')
-        self.THREAD.grid(row=0,column=3)
-        self.page_label = Label(self,text='查询页数').grid(row=0,column=4)
+        self.THREAD.grid(row=0,column=4)
+        self.page_label = Label(self,text='查询页数:').grid(row=0,column=5)
         self.page_choice = StringVar(self)
         self.page_choice.set('1')
         self.PAGE = OptionMenu(self,self.page_choice,'1','2','3','4','5','10','15','20','30','50','80','100','200','300','500','1000')
-        self.PAGE.grid(row=0,column=5)
-        self.mode_label = Label(self,text='模式').grid(row=0,column=6)
-        self.mode_choice = StringVar(self)
-        self.mode_choice.set('主机搜索')
-        self.MODE = OptionMenu(self,self.mode_choice,'主机搜索','域名/IP','个人信息')
-        self.MODE.grid(row=0,column=7)
-        self.START = Button(self,text='一键查询',command=self.thread)
-        self.START.grid(row=0,column=8)
-        self.QUERY = Entry(self, width=18,borderwidth=1)
-        self.QUERY.grid(row=1,column=3,columnspan=4)
-        self.file_label = Label(self,text='存储文件名').grid(row=1,column=7)
-        self.FILE = Entry(self,width=8,borderwidth=1)
-        self.FILE.grid(row=1,column=8)
+        self.PAGE.grid(row=0,column=6)
+        self.mode_label = Label(self,text='模式:').grid(row=0,column=7)
+        self.query_mode_choice = StringVar(self)
+        self.query_mode_choice.set('主机搜索')
+        self.MODE = OptionMenu(self,self.query_mode_choice,'主机搜索','域名/IP','个人信息')
+        self.MODE.grid(row=0,column=8)
+        self.START = Button(self,text='查询',command=self.thread)
+        self.START.grid(row=0,column=9)
+        self.QUERY = Entry(self, width=35,borderwidth=1)
+        self.QUERY.grid(row=1,column=4,columnspan=8)
+        self.file_label = Label(self,text='存储文件名:').grid(row=2,column=3)
+        self.FILE = Entry(self,width=35,borderwidth=1)
+        self.FILE.grid(row=2,column=4,columnspan=8)
         self.TREEVIEW = Treeview(self,show='headings')
-        self.TREEVIEW.grid(row=2,column=0,columnspan=9)
+        self.TREEVIEW.grid(row=3,column=0,columnspan=10)
         self.TREEVIEW['columns'] = ("ID","IP","PORT/DOMAIN","OS")
         self.TREEVIEW.column("ID",width=50)
-        self.TREEVIEW.column("IP",width=200)
-        self.TREEVIEW.column("PORT/DOMAIN",width=150)
-        self.TREEVIEW.column("OS",width=200)
+        self.TREEVIEW.column("IP",width=220)
+        self.TREEVIEW.column("PORT/DOMAIN",width=200)
+        self.TREEVIEW.column("OS",width=220)
         self.TREEVIEW.heading("ID",text="ID")
         self.TREEVIEW.heading("IP",text="IP")
         self.TREEVIEW.heading("PORT/DOMAIN",text="PORT/DOMAIN")
         self.TREEVIEW.heading("OS",text="OS")
-        self.LOG = Text(self,relief=SOLID,borderwidth=1,height=10,width=86)
-        self.LOG.grid(row=3,column=0,columnspan=9)
+        self.LOG = Text(self,relief=SOLID,borderwidth=1,height=15,width=98)
+        self.LOG.grid(row=4,column=0,columnspan=10)
 
     def delete_tree(self,tree):
         x = tree.get_children()
@@ -72,45 +80,51 @@ class Application(Frame):
                 access_token = f.read().strip('\n')
             self.log_insert("Load file 'access_token.txt' successfully.\n")
         except FileNotFoundError:
-            self.log_insert("Fail to find access_token.txt. Need to Login now.\n")
-            url = 'https://api.zoomeye.org/user/login'
-            data = {
-                'username':self.USERNAME.get().strip(),
-                'password':self.PASSWORD.get().strip()
-            }
-            encoded_data = json.dumps(data)
-            resp_json = requests.post(url, encoded_data).json()
-            try:
-                access_token = resp_json['access_token']
-            except:
-                self.log_insert(f'[-] Login fail. {resp_json["message"]}\n')
-            else:
-                self.log_insert('[+] Login success!\n')
-                with open("access_token.txt", "w") as f:
-                    f.write(access_token)
+            if self.login_mode_choice.get() == "账号密码":
+                self.log_insert("Fail to find access_token.txt. Need to Login now.\n")
+                url = 'https://api.zoomeye.org/user/login'
+                data = {
+                    'username':self.USERNAME.get().strip(),
+                    'password':self.PASSWORD.get().strip()
+                }
+                encoded_data = json.dumps(data)
+                resp_json = requests.post(url, encoded_data).json()
+                try:
+                    access_token = resp_json['access_token']
+                except:
+                    self.log_insert(f'[-] Login fail. {resp_json["message"]}\n')
+                else:
+                    self.log_insert('[+] Login success!\n')
+                    with open("access_token.txt", "w") as f:
+                        f.write(access_token)
+                self.headers = {
+                    'Authorization':'JWT ' + access_token
+                }
+                print(self.headers['Authorization'])
+                self.log_insert(f"[+] Access_Token: {self.headers['Authorization']}\n")
 
-        self.headers = {
-            'Authorization':'JWT ' + access_token
-        }
-        print(self.headers)
-        self.log_insert(f"Access_Token: {self.headers['Authorization']}\n")
-    
+            elif self.login_mode_choice.get() == "API-KEY":
+                self.headers = {
+                    "API-KEY":self.API.get().strip()
+                }
+                self.log_insert(f"[+] API-KEY: {self.headers['API-KEY']}\n")
+
     def thread(self):
-        if self.QUERY.get() != "" and self.FILE.get() != "" or self.mode_choice.get() == "个人信息":
+        if self.QUERY.get() != "" and self.FILE.get() != "" or self.query_mode_choice.get() == "个人信息":
             t1 = Thread(target=self.run,daemon=True)
             t1.start()
         else:
-            messagebox.showerror(title='Error',message='Query or FilePath empty!')
+            messagebox.showerror(title='Error',message='[!] Query or FilePath empty!')
 
     def run(self):
-        if self.mode_choice.get() != '个人信息':
+        if self.query_mode_choice.get() != '个人信息':
             self.delete_tree(self.TREEVIEW)
         self.info_list = []
         self.login()
         self.log_insert('Start searching...\n')
         query = self.QUERY.get().replace(" ",'%20')
 
-        if self.mode_choice.get() == '主机搜索':
+        if self.query_mode_choice.get() == '主机搜索':
             self.host_search(query=query,page=self.page_choice.get(),thread=int(self.thread_choice.get()))
             j = 1
             with open(self.FILE.get(),"w") as f:
@@ -120,7 +134,7 @@ class Application(Frame):
                     f.write(f"{each_dic['ip']}:{each_dic['port']},{each_dic['country']},{each_dic['os']},{each_dic['hostname']}\n")
                     j += 1
                     
-        if self.mode_choice.get() == '域名/IP':
+        if self.query_mode_choice.get() == '域名/IP':
             self.domain_ip(query=query,page=self.page_choice.get(),thread=int(self.thread_choice.get()))
             j = 1
             with open(self.FILE.get(),"w") as f:
@@ -130,12 +144,11 @@ class Application(Frame):
                     f.write(f"{each_dic['ip']} {each_dic['name']}\n")
                     j += 1
 
-        if self.mode_choice.get() == '个人信息':
+        if self.query_mode_choice.get() == '个人信息':
             self.resource(mode="complete")
-
-        if self.mode_choice.get() != "个人信息":
+        else:
             self.log_insert(f'Complete information has been stored into {self.FILE.get()}.\n')
-            self.resource(mode="easy")
+            self.resource(mode="easy")            
 
     def host_search(self, query, page, thread):
         with ThreadPoolExecutor(thread) as t:
@@ -207,9 +220,9 @@ class Application(Frame):
 
 if __name__=='__main__':
     root = Tk()
-    root.geometry('640x404+450+100')
-    root.maxsize(width=640,height=404)
-    root.minsize(width=640,height=404)
-    root.title('ThunderSearch v1.3')
+    root.geometry('718x497+350+100')
+    root.maxsize(width=718,height=497)
+    root.minsize(width=718,height=497)
+    root.title('ThunderSearch v1.5')
     Application(master=root)
     root.mainloop()
