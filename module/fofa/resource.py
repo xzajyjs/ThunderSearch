ed2dabad108be68106484624022d1adeb08e959f
xzajyjs <xuziang16@gmail.com>
2023-08-12 20:33:48 +0800

    fix fofa bug

--- module/fofa/resource.py
+++ module/fofa/resource.py
@@ -1,6 +1,7 @@
 import requests
 
-def fofa_search_resource(email,key):
+
+def fofa_search_resource(email, key):
     params = {
         "email": email,
         "key": key
@@ -8,8 +9,9 @@ def fofa_search_resource(email,key):
     try:
         resp = requests.get('https://fofa.info/api/v1/info/my', data=params)
         data = resp.json()
+        if "'error': True" in str(data):
+            return f"[!] Error: {data['errmsg']}\n"
         string = f"======== Fofa ========\n[+] Email: {data['email']}\n[+] Username: {data['username']}\n[+] Fcoin: {data['fcoin']}\n[+] isVip: {data['isvip']}\n[+] Vip_level: {data['vip_level']}\n[+] is_Verified: {data['is_verified']}\n[+] Avatar: {data['avatar']}\n========================\n"
         return string
     except Exception as e:
         return e
-    
